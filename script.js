44fae238db71386919d4ce33ac72e16123ac9dba
Trat1100 <itsaffan27@outlook.com>
2023-07-18 08:21:44 -0500

    added decryption

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function encrypt() {
     var encryptedText = sjcl.encrypt(key, inputText);
     document.getElementById("outputText").value = encryptedText;
 }
+
+function decrypt() {
+    var inputText = document.getElementById("inputText").value;
+    var key = "secret_key"; // It is also a secret key
+    var decryptedText = sjcl.decrypt(key, inputText);
+    document.getElementById("outputText").value = decryptedText;
+}
