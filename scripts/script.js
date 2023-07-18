ff9c33bb15a486a43a47dc027d9cd7e46b4d116a
Aarush <justforminecraft255@gmail.com>
2023-07-18 21:46:17 +0530

    buttons done and layout made responsive

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,26 @@ function encrypt() {
     var key = "secret_key"; // It is a secret key
     var encryptedText = sjcl.encrypt(key, inputText);
     document.getElementById("outputText").value = encryptedText;
+
+    let btn = document.querySelectorAll(".btn");
+btn.forEach(btn => {
+    btn.onclick = function(e){
+        let x = e.pageX - e.target.offsetLeft;
+        let y = e.pageY - e.target.offsetTop;
+
+        let color = "#" + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+
+        let ripples = document.createElement("span");
+        ripples.style.left = x + "px";
+        ripples.style.top = y + "px";
+        ripples.style.borderColor = color;
+        this.appendChild(ripples);
+
+        setTimeout(() => {
+            ripples.remove()
+        }, 2000);
+    }
+})
 }
 
 function decrypt() {
@@ -10,4 +30,24 @@ function decrypt() {
     var key = "secret_key"; // It is also a secret key
     var decryptedText = sjcl.decrypt(key, inputText);
     document.getElementById("outputText").value = decryptedText;
+
+    let btn = document.querySelectorAll(".btn");
+btn.forEach(btn => {
+    btn.onclick = function(e){
+        let x = e.pageX - e.target.offsetLeft;
+        let y = e.pageY - e.target.offsetTop;
+
+        let color = "#" + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+
+        let ripples = document.createElement("span");
+        ripples.style.left = x + "px";
+        ripples.style.top = y + "px";
+        ripples.style.borderColor = color;
+        this.appendChild(ripples);
+
+        setTimeout(() => {
+            ripples.remove()
+        }, 2000);
+    }
+})
 }
